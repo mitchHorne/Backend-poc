5bf7103e406aa9d13020bd50e9f991f89e53b4bb
Mitchell Horne <mitchellhorne@Mitchells-Air.lan>
2025-02-13 13:51:43 +0200

    Get ready for deploy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Koa from "koa";
 import router from "./routes.js";
+import serve from "koa-static";
 
 const app = new Koa();
-
+app.use(serve("dist/"));
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
